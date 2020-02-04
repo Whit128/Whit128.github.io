21e0b64a2ffb0e701557ce3ea3139add4ef602da
Dave <contact@davidwhitcome.com>
2020-02-03 22:58:37 -0600

    logic in typescript

--- src/app/hero-header/hero-header.component.ts
+++ src/app/hero-header/hero-header.component.ts
@@ -1,30 +1,24 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero-header',
   templateUrl: './hero-header.component.pug',
   styleUrls: ['./hero-header.component.scss']
 })
-export class HeroHeaderComponent implements OnInit {
+export class HeroHeaderComponent {
   Name: string = "David Whitcome";
   Tag: string = "Software Developer";
 
-  getHeaderHeight(): number {
-    let newHeight = 300 - window.pageYOffset;
-    return newHeight < 0 ? 0 : newHeight;
+  originalHeight: number = 300;
+  headerHeight: number;
+  opacity: number;
+  h2Height: number;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.headerHeight = Math.max(0, this.originalHeight - window.pageYOffset);
+    this.opacity = Math.max(0, 1 - window.pageYOffset/(this.originalHeight/1.2));
+    this.h2Height = Math.max(0, 22 - window.pageYOffset/10);
   }
 
-  getHeight(): number {
-    let newHeight = 22 - window.pageYOffset/10;
-    return newHeight < 0 ? 0 : newHeight;
-  }
-
-  getOpacity(denominator: number): number {
-    let newOpacity = 1 - window.pageYOffset/denominator;
-    return newOpacity < 0 ? 0 : newOpacity;
-  }
-
-  ngOnInit() {
-    window.addEventListener('scroll', function (){});
-  }
 } 
