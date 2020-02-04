ce347180e61b6f34b4f3741d9cfcc40891ad8750
Dave <contact@davidwhitcome.com>
2020-02-03 21:55:30 -0600

    h2 - get smaller and reduce opacity

--- src/app/hero-header/hero-header.component.ts
+++ src/app/hero-header/hero-header.component.ts
@@ -14,7 +14,15 @@ export class HeroHeaderComponent implements OnInit {
     return newHeight < 0 ? 0 : newHeight;
   }
 
-  getOpacity = (denominator: number): number => 1 - window.pageYOffset/denominator;
+  getHeight(): number {
+    let newHeight = 22 - window.pageYOffset/10;
+    return newHeight < 0 ? 0 : newHeight;
+  }
+
+  getOpacity(denominator: number): number {
+    let newOpacity = 1 - window.pageYOffset/denominator;
+    return newOpacity < 0 ? 0 : newOpacity;
+  }
 
   ngOnInit() {
     window.addEventListener('scroll', function (){});
