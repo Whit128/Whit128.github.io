f6acc0ee3c8d52784335ed3757d323a760c5dd51
Dave Whitcome <contact@davidwhitcome.com>
2021-03-20 01:38:28 -0500

    basic right column

--- reactjs/davidwhitcome.com/src/main-card/main-card.tsx
+++ reactjs/davidwhitcome.com/src/main-card/main-card.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './main-card.scss'
 import LeftColumn from './left-column/left-column'
+import RightColumn from './right-column/right-column'
 
 export default class MainCard extends React.Component {
   render = () => (
     <div className="section">
       <div className="flex-content inner">
         <LeftColumn/>
+        <RightColumn/>
         {/* <app-left-column></app-left-column>
         <app-right-column></app-right-column> */}
       </div>
