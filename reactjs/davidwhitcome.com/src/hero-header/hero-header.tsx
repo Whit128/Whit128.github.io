e4fe93b19eb0b1fae4aefbc0f38b20aa065c01c2
Dave Whitcome <contact@davidwhitcome.com>
2021-03-21 20:54:20 -0500

    get div using ref

--- reactjs/davidwhitcome.com/src/hero-header/hero-header.tsx
+++ reactjs/davidwhitcome.com/src/hero-header/hero-header.tsx
@@ -2,44 +2,46 @@ import React from 'react'
 import './hero-header.scss'
 
 interface IState {
-  name: string,
-  tag: string,
+    name: string,
+    tag: string,
+    headerBackground: React.RefObject<HTMLDivElement>
 }
 
 export default class HeroHeader extends React.Component<{}, IState> {
-  state = {
-    name: "David Whitcome",
-    tag: "Software Developer"
-  };
+    state = {
+        name: "David Whitcome",
+        tag: "Software Developer",
+        headerBackground: React.createRef<HTMLDivElement>()
+    };
 
-  constructor(props: {}) {
-    super(props);
-    this.ParallaxAnimation(); // Set header background to correct position for some browsers.
-    window.addEventListener('scroll', this.ParallaxAnimation);
-  }
-
-  render = () => (
-    <div id="header">
-      <div id="header-background-wrapper">
-        <div id="header-background"></div>
-      </div>
-      <div className="ie10-fix">
-        <div id="header-centered">
-          <h1>{this.state.name}</h1>
-          <h2>{this.state.tag}</h2>
-        </div>
-      </div>
-    </div>
-  );
-
-  ParallaxAnimation() {
-    var headerBackground = document.getElementById("header-background-wrapper");
-    if (headerBackground != null) {
-      var backgroundPosition: number = window.pageYOffset / 4;
-      if (backgroundPosition >= 0)
-        headerBackground.style.marginTop = backgroundPosition + "px";
-      else
-        headerBackground.style.marginTop = "0px";
+    constructor(props: {}) {
+        super(props);
+        this.ParallaxAnimation(this.state.headerBackground.current); // Set header background to correct position for some browsers.
+        window.addEventListener('scroll', () =>
+            this.ParallaxAnimation(this.state.headerBackground.current));
     }
-  }
+
+    render = () => (
+        <div id="header">
+        <div id="header-background-wrapper" ref={this.state.headerBackground}>
+            <div id="header-background"></div>
+        </div>
+        <div className="ie10-fix">
+            <div id="header-centered">
+            <h1>{this.state.name}</h1>
+            <h2>{this.state.tag}</h2>
+            </div>
+        </div>
+        </div>
+    );
+
+    ParallaxAnimation(element: HTMLDivElement | null) {
+        if (element == null)
+            return;
+        var backgroundPosition: number = window.pageYOffset / 4;
+        if (backgroundPosition >= 0)
+            element.style.marginTop = backgroundPosition + "px";
+        else
+            element.style.marginTop = "0px";
+    }
 };
