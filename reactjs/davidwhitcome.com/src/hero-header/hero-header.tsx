50fb65c862639ea78650e2f47fb52771eb800f3e
Dave Whitcome <contact@davidwhitcome.com>
2021-03-21 20:39:20 -0500

    basic scrolling animation

--- reactjs/davidwhitcome.com/src/hero-header/hero-header.tsx
+++ reactjs/davidwhitcome.com/src/hero-header/hero-header.tsx
@@ -3,7 +3,7 @@ import './hero-header.scss'
 
 interface IState {
   name: string,
-  tag: string
+  tag: string,
 }
 
 export default class HeroHeader extends React.Component<{}, IState> {
@@ -11,6 +11,12 @@ export default class HeroHeader extends React.Component<{}, IState> {
     name: "David Whitcome",
     tag: "Software Developer"
   };
+
+  constructor(props: {}) {
+    super(props);
+    this.ParallaxAnimation(); // Set header background to correct position for some browsers.
+    window.addEventListener('scroll', this.ParallaxAnimation);
+  }
 
   render = () => (
     <div id="header">
@@ -25,4 +31,15 @@ export default class HeroHeader extends React.Component<{}, IState> {
       </div>
     </div>
   );
+
+  ParallaxAnimation() {
+    var headerBackground = document.getElementById("header-background-wrapper");
+    if (headerBackground != null) {
+      var backgroundPosition: number = window.pageYOffset / 4;
+      if (backgroundPosition >= 0)
+        headerBackground.style.marginTop = backgroundPosition + "px";
+      else
+        headerBackground.style.marginTop = "0px";
+    }
+  }
 };
